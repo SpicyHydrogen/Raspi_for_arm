f6bca07cf13af99f62932dae65d7953aea6a67d1
Tanagid Rakwongrit <115927200+SpicyHydrogen@users.noreply.github.com>
2023-09-07 09:33:38 +0700

    Update get_pi_requirements.sh

--- get_pi_requirements.sh
+++ get_pi_requirements.sh
@@ -4,6 +4,9 @@
 
 sudo apt-get update
 sudo apt-get dist-upgrade
+sudo pip3 install virtualenv
+python3 -m venv RaspiProject-env
+source RaspiProject-env/Scripts/activate
 sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
